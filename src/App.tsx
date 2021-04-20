67706e1ea4019b78ea1ef4fec1587853e234b6b5
Dylan <d.barva5402@gmail.com>
2021-04-19 20:57:31 -0700

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Project from './content/Project';
-import { Container, Jumbotron } from 'react-bootstrap';
+import { Container, Jumbotron, Nav, Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <Container>
-      <Jumbotron fluid style={{backgroundColor : "blue"}}>
-          <h1 style={{}}>Dylan Barva</h1>
-      </Jumbotron>
-      {/* <Project projectName = "Taco" projectDescription = "Potato" projectTimeline = "Now"></Project> */}
-      
-    </Container>
+      <Navbar fixed="top" color = "faded" bg="dark" variant="dark">
+        
+        <Navbar.Brand>Dylan Barva</Navbar.Brand>
+        <Nav.Link style={{color: 'white'}} href="#github">Github</Nav.Link>
+        <Nav.Link style={{color: 'white'}} >Projects</Nav.Link>
+        <Nav.Link style={{color: 'white'}}>Courses</Nav.Link>
+      </Navbar>
+      /* <Project projectName = "Taco" projectDescription = "Potato" projectTimeline = "Now"></Project> */
   );
 }
 
